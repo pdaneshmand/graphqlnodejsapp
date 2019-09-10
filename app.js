d023ae42409cce7fb8da47ddd60e332e9714296c
Pourdad Daneshmand <pourdad.daneshmand@gmail.com>
2019-09-11 01:39:01 +0430

    add cors package

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+//cors
+app.use(cors());
 
 //connect to mongo atlas
 mongoose.connect(
